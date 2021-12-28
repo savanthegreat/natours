71a33bfa61fd439b602d0857a83f5e68acce940e
Savan Barbhaya <savanthegreat@gmail.com>
2021-12-28 21:27:24 +0530

    API development

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require('express');
-
+const fs = require('fs');
 const port = 3000;
+
 const app = express();
+app.use(express.json());
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome', project: 'natours' });
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    Status: 'success',
+    Count: tours.length,
+    Data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.status(404).json({ message: 'You Can Post HERE !!', project: 'natours' });
+app.post('/api/v1/tours', (req, res) => {
+  const ID = tours.length + 1;
+  const toursUpdated = Object.assign({ ID: ID }, req.body);
+  tours.push(toursUpdated);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    () => {
+      console.log('written');
+    }
+  );
+  res.status(201).send('Done');
 });
+
 app.listen(port, () => {
   console.log('listening on port ' + port);
 });
