2704bbcb1c7480e78e8efffdcd012c7f8f792207
Savan Barbhaya <savanthegreat@gmail.com>
2021-12-29 14:04:45 +0530

    Developing routers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+const { STATUS_CODES } = require('http');
+const { isNull } = require('util');
 const port = 3000;
 
 const app = express();
@@ -8,7 +10,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const GetAllTours = (req, res) => {
   res.status(200).json({
     Status: 'success',
     Count: tours.length,
@@ -16,21 +18,130 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
-  const ID = tours.length + 1;
-  const toursUpdated = Object.assign({ ID: ID }, req.body);
+const GetTour = (req, res) => {
+  const tourID = req.params.id * 1;
+  const tourDetail = tours.find((el) => el.id === tourID);
+
+  if (!tourDetail) {
+    return res.status(404).json({
+      Status: 'failed',
+      message: 'No tour with this ID is available in Database',
+    });
+  }
+
+  res.status(200).json({
+    Status: 'success',
+    Data: {
+      tour: tourDetail,
+    },
+  });
+};
+
+const PostTour = (req, res) => {
+  const ID_new = tours.length + 1;
+  const toursUpdated = Object.assign(
+    {
+      ID: ID_new,
+    },
+    req.body
+  );
   tours.push(toursUpdated);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    () => {
-      console.log('written');
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          NewTour: toursUpdated,
+        },
+      });
     }
   );
-  res.status(201).send('Done');
-});
+};
+
+const UpdateTour = (req, res) => {
+  const tourID = req.params.id * 1;
+
+  if (tourID > tours.length) {
+    return res.status(404).json({
+      Status: 'failed',
+      message: 'No tour with this ID is available in Database',
+    });
+  }
+  res.status(200).json({
+    Status: 'success',
+    Data: {
+      tour: `We have Updated tour with ID ${tourID}`,
+    },
+  });
+};
+
+const DeleteTour = (req, res) => {
+  const tourID = req.params.id * 1;
+
+  if (tourID > tours.length) {
+    return res.status(404).json({
+      Status: 'failed',
+      message: 'No tour with this ID is available in Database',
+    });
+  }
+  res.status(204).json({
+    Status: 'success',
+    Data: null,
+  });
+};
+
+const GetAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is yet to be implemented',
+  });
+};
+
+const PostUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is yet to be implemented',
+  });
+};
+
+const GetUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is yet to be implemented',
+  });
+};
+
+const UpdateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is yet to be implemented',
+  });
+};
+
+const DeleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This is yet to be implemented',
+  });
+};
+
+app.route('/api/v1/tours').get(GetAllTours).post(PostTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(GetTour)
+  .patch(UpdateTour)
+  .delete(DeleteTour);
+
+app.route('/api/v1/users').get(GetAllUsers).post(PostUser);
+app
+  .route('/api/v1/users/:id')
+  .get(GetUser)
+  .patch(UpdateUser)
+  .delete(DeleteUser);
 
 app.listen(port, () => {
   console.log('listening on port ' + port);
